9097727ec0d82df46fb41382fdc4a7e2fbe02ecc
ajayshenoyh <ajayshenoyh@gmail.com>
2017-03-22 09:58:42 -0700

    Images, Label text
    Add images in project directory and respond changes to label text on
    toggle switch

--- bmi/ViewController.swift
+++ bmi/ViewController.swift
@@ -5,7 +5,7 @@ Class: Spring 2017 CPSC 411-01
 Description: This swift file is for all the view-controller interaction for BMI Calculator. Based on Metric switch it chooses the units that will be used for the BMI calculation 
 and outputs the result with approx visual physic of the person and provide me him tips for normal bmi if he doesnt't fall under normal category.
 */
-// Line 62, Source:http://stackoverflow.com/questions/39546856/how-to-open-an-url-in-swift3
+// Source:http://stackoverflow.com/questions/39546856/how-to-open-an-url-in-swift3
 //  ViewController.swift
 //  bmi
 //
@@ -49,15 +49,18 @@ class ViewController: UIViewController{
         if metricSwitch.isOn{
             hTextField.placeholder="in cm"
             wTextField.placeholder="in Kg"
-            hTextField.text=""
-            wTextField.text=""
+            hLabel.text="Height in Cm"
+            wLabel.text="Weight in Kg"
+           // wTextField.text=""
         }
         else
         {
             hTextField.placeholder="in inches"
             wTextField.placeholder="in lbs"
-            hTextField.text=""
-            wTextField.text=""
+            hLabel.text="Height in inches"
+            wLabel.text="Weight in lbs"
+            //hTextField.text=""
+            //wTextField.text=""
         }
     }
     func open(scheme: String) {
